b6eb402f635891dd8b68546511e5275b5636b0c1
chdineshbabu <chdinesh4128@gmail.com>
2024-03-05 15:01:25 +0530

    Router fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import './App.css';
 import Login from './pages/Login.js';
-import Profile from './pages/Profile.js'
+import Profile from './pages/Profile.js';
 import Dashboard from './pages/Dashboard.js';
-import Signup from './pages/Signup.js'
+import Signup from './pages/Signup.js';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />}>
-          <Route path="profile" element={<Profile />} />
-          <Route path="/dash" element={<Dashboard />} />
-          <Route path="Signup" element={<Signup />} />
-        </Route>
+        <Route path="/" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/dash" element={<Dashboard />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
