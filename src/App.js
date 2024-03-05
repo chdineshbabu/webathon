65fb59a2a89a331f7178cc6e1f8259d7cfbcabd5
chdineshbabu <chdinesh4128@gmail.com>
2024-03-05 14:08:09 +0530

    Initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div className='text-xl '>
+      Let's start here
+    </div>
+    </div>
+  );
+}
+
+export default App;
