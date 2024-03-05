ae77705cacd4f24936b57a7c073e7a8f980110b3
chdineshbabu <chdinesh4128@gmail.com>
2024-03-05 14:55:38 +0530

    Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
+import Login from './pages/Login.js';
+import Profile from './pages/Profile.js'
+import Dashboard from './pages/Dashboard.js';
+import Signup from './pages/Signup.js'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <div className='text-xl '>
-        Let's start here
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}>
+          <Route path="profile" element={<Profile />} />
+          <Route path="/dash" element={<Dashboard />} />
+          <Route path="Signup" element={<Signup />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
